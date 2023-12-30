4e6668bd64cf82f3a6ac038f60905bf293f194cb
Yash Sehgal <yashsehgal.work@gmail.com>
2023-12-30 17:27:11 +0530

    ⬆️ Version update <> Style config fixes

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,31 +1,7 @@
 import React, { ReactNode } from "react";
 import { cn } from "../../utils";
-import { cva, type VariantProps } from 'class-variance-authority';
 
-const buttonVariants = cva(
-  "craftbookComponent border flex flex-row items-center justify-center font-medium",
-  {
-    variants: {
-      variant: {
-        primary: "bg-product-primary text-white border-transparent hover:bg-product-primary-dimmed",
-        secondary: "bg-product-secondary text-white border-transparent hover:bg-product-secondary-dimmed",
-        solid: "bg-product-dark text-white border-transparent",
-        outline: "bg-white text-product-dark border-neutral-200 hover:bg-product-light-dimmed",
-      },
-      size: {
-        sm: "px-3 py-1.5 text-sm gap-1",
-        md: "px-4 py-2 gap-1.5",
-        lg: "px-5 py-2.5 text-lg gap-2"
-      }
-    },
-    defaultVariants: {
-      variant: "primary",
-      size: "md"
-    }
-  }
-)
-
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary" | "solid" | "outline";
   size?: "sm" | "md" | "lg";
   strech?: boolean;
@@ -36,8 +12,8 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 const Button: React.FunctionComponent<ButtonProps> = ({
   className,
   children,
-  variant,
-  size,
+  variant = "primary",
+  size = "md",
   icon = null,
   iconDirection = "left",
   strech = false,
@@ -45,7 +21,20 @@ const Button: React.FunctionComponent<ButtonProps> = ({
 }) => {
   return (
     <button
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(
+        // default button styles
+        "craftbookComponent border flex flex-row items-center justify-center font-medium",
+        // variant based button styles
+        variant === "primary" && "bg-product-primary text-white border-transparent hover:bg-product-primary-dimmed",
+        variant === "secondary" && "bg-product-secondary text-white border-transparent hover:bg-product-secondary-dimmed",
+        variant === "solid" && "bg-product-dark text-white border-transparent",
+        variant === "outline" && "bg-white text-product-dark border-neutral-200 hover:bg-product-light-dimmed",
+        // size based button styles
+        size === "sm" && "px-3 py-1.5 text-sm gap-1",
+        size === "md" && "px-4 py-2 gap-1.5",
+        size === "lg" && "px-5 py-2.5 text-lg gap-2",
+        className
+      )}
       {...props}
     >
       {iconDirection === "left" && icon}

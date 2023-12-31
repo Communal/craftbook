ea5c9190b984e2f6df17139b231aaea64cc2cc28
Yash Sehgal <yashsehgal.work@gmail.com>
2023-12-31 11:42:18 +0530

    💄 Add button component and styles to storybook docs

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,7 +1,45 @@
+import { cn } from "@/utils";
 
-const Button: React.FunctionComponent = () => {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "solid" | "outline";
+  size?: "sm" | "md" | "lg";
+  strech?: boolean;
+  icon?: React.ReactNode;
+  iconDirection?: "left" | "right";
+}
+
+const Button: React.FunctionComponent<ButtonProps> = ({
+  className,
+  children,
+  variant = "primary",
+  size = "md",
+  icon = null,
+  iconDirection = "left",
+  strech = false,
+  ...props
+}) => {
   return (
-    <button></button>
+    <button
+      className={cn(
+        // default button styles
+        "craftbookComponent border flex flex-row items-center justify-center font-medium",
+        // variant based button styles
+        variant === "primary" && "bg-product-primary text-white border-transparent hover:bg-product-primary-dimmed",
+        variant === "secondary" && "bg-product-secondary text-white border-transparent hover:bg-product-secondary-dimmed",
+        variant === "solid" && "bg-product-dark text-white border-transparent",
+        variant === "outline" && "bg-white text-product-dark border-neutral-200 hover:bg-product-light-dimmed",
+        // size based button styles
+        size === "sm" && "px-3 py-1.5 text-sm gap-1 rounded-md",
+        size === "md" && "px-4 py-2 gap-1.5 rounded-lg",
+        size === "lg" && "px-5 py-2.5 text-lg gap-2 rounded-xl",
+        className
+      )}
+      {...props}
+    >
+      {iconDirection === "left" && icon}
+      {children}
+      {iconDirection === "right" && icon}
+    </button>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "product-primary": "#FF6D3F",
+        "product-primary-light": "#FF9877",
+        "product-primary-dimmed": "#FF480E",
+        "product-secondary": "#3F80FF",
+        "product-secondary-light": "#75A4FF",
+        "product-secondary-dimmed": "#256FFF",
+        "product-light": "#FFFFFF",
+        "product-light-dimmed": "#FCFCFC",
+        "activity-red": "#FF4545",
+        "activity-red-light": "#FF8181",
+        "activity-red-dimmed": "#DD1818",
+        "product-dark": "#2F2F2F",
+      }
+    },
   },
   plugins: [],
 }

7a8dd03cf9a316018b24272706c45f825d681d18
Yash Sehgal <yashsehgal.work@gmail.com>
2023-12-31 12:12:58 +0530

    ⚒️ Add config for component & package build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/components/index.tsx'),
+      name: "Communal Craftbook",
+      fileName: "communal-craftbook",
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+});

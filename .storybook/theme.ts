e491c38d3121b52ba86a6c7d67d79c7e81671051
Yash Sehgal <yashsehgal.work@gmail.com>
2024-01-01 11:46:27 +0530

    🔧 Add custom storybook theme config

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Craftbook',
+  brandUrl: 'https://github.com/Communal',
+  // brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+  brandTarget: '_self',
+});

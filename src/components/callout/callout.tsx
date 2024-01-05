c978d5879e1673dc239328ca6755ac69b682c2ca
Yash Sehgal <yashsehgal.work@gmail.com>
2024-01-05 16:26:16 +0530

    ⬆️ Bump to v1.2.2 (dev)

--- src/components/callout/callout.tsx
+++ src/components/callout/callout.tsx
@@ -11,27 +11,27 @@ const CalloutConfig = {
   message: {
     icon: <Info className="w-4 h-5" />,
     style: 'bg-neutral-100 text-neutral-500',
-    highlighter: "bg-neutral-500",
+    highlighter: 'bg-neutral-500',
   },
   success: {
     icon: <CheckCircle2 className="w-4 h-4" />,
     style: 'bg-green-100 text-green-600',
-    highlighter: "bg-green-600",
+    highlighter: 'bg-green-600',
   },
   info: {
     icon: <Info className="w-4 h-5" />,
     style: 'bg-blue-100 text-blue-700',
-    highlighter: "bg-blue-700",
+    highlighter: 'bg-blue-700',
   },
   warning: {
     icon: <AlertTriangle className="w-4 h-5" />,
     style: 'bg-yellow-100 text-yellow-700',
-    highlighter: "bg-yellow-700",
+    highlighter: 'bg-yellow-700',
   },
   error: {
     icon: <AlertTriangle className="w-4 h-5" />,
     style: 'bg-red-100 text-red-600',
-    highlighter: "bg-red-600",
+    highlighter: 'bg-red-600',
   },
 };
 
@@ -48,10 +48,12 @@ const Callout = forwardRef<HTMLDivElement, CalloutProps>(
           CalloutConfig[type].style,
           className,
         )}
-        {...props}
-      >
-        <div className={cn("h-auto w-1", CalloutConfig[type].highlighter)} />
-        <div className={cn("py-3 pl-3 pr-6 text-xs flex flex-row items-center gap-2 leading-snug font-medium")}>
+        {...props}>
+        <div className={cn('h-auto w-1', CalloutConfig[type].highlighter)} />
+        <div
+          className={cn(
+            'py-3 pl-3 pr-6 text-xs flex flex-row items-center gap-2 leading-snug font-medium',
+          )}>
           <span className="callout-icon-wrapper w-fit h-fit">
             {CalloutConfig[type].icon}
           </span>

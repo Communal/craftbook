4af1cffffee83540ca2197257c9f5f9ce3fbeeab
Yash Sehgal <yashsehgal.work@gmail.com>
2024-01-20 10:17:33 +0530

    ⬆️ Bump to v1.5.1 (stable)

--- src/components/tab-menu/tab-menu.tsx
+++ src/components/tab-menu/tab-menu.tsx
@@ -16,19 +16,26 @@ interface TabMenuOptionProps
 }
 
 const TabMenu = forwardRef<HTMLDivElement, TabMenuProps>(
-  ({ className, options, currentOption = '', stretch = false, ...props }, ref) => {
+  (
+    { className, options, currentOption = '', stretch = false, ...props },
+    ref,
+  ) => {
     const [selectedOption, setSelectedOption] = useState<string>(
       currentOption || options[0],
     );
 
     return (
-      <Box className={cn("border-b", stretch && "flex flex-row items-center justify-center")}>
+      <Box
+        className={cn(
+          'border-b',
+          stretch && 'flex flex-row items-center justify-center',
+        )}>
         <Grid
           ref={ref}
           className={cn('w-fit', className)}
           cols={options.length}
           alignItems="center"
-          justifyContent='center'
+          justifyContent="center"
           {...props}>
           {options.map((option, index) => {
             return (
@@ -58,7 +65,7 @@ const TabMenuOption = forwardRef<HTMLButtonElement, TabMenuOptionProps>(
           'tab-menu-option px-3 py-1.5 transition-all w-fit',
           !isSelected && 'hover:border-b-2 hover:border-black/10',
           isSelected &&
-          'border-b-2 border-b-black drop-shadow-lg before:outline-none',
+            'border-b-2 border-b-black drop-shadow-lg before:outline-none',
           className,
         )}
         aria-label={label}
